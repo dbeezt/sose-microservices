7bb7c92bf10cd6dad1082d461bbc84947571a0b2
dbeezt <dbrereton1995@gmail.com>
2020-05-21 22:37:27 +0100

    Unauthenticated database connection in place

--- users/server.js
+++ users/server.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose')
 const app = require('./src/app');
-const port = 3000
 
 if (process.env.MONGO_DB_URI){
     dbConn = process.env.MONGO_DB_URI;
 } else {
-    dbConn = 'mongodb://localhost:27017/sose-movies'
+    dbConn = 'mongodb://users-mongodb:27111'
+    console.log("bleugh!");
 }
 
-mongoose.connect(dbConn);
+// user + pass to be added
+console.log(dbConn);
+mongoose.connect(dbConn, { useNewUrlParser: true, 
+                           autoIndex: false,
+                           useFindAndModify: false,
+                           useUnifiedTopology: true })
+.then(() => { 
+    console.log("User DB Connection SUCCESSFUL");
+}).catch((err) => {
+    console.log("User DB Connection ERROR: ", err);
+});
 
-app.listen(port, () => {
-    console.log('users' + 'active')
-})
+app.listen(3000, () => {
+    console.log('users listening to 3000')
+});
