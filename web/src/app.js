6889158ce537993f5b55b4a3dbc35dcc16c7ed02
dbeezt <dbrereton1995@gmail.com>
2020-05-22 09:28:03 +0100

    Render index.pug with example User var

--- web/src/app.js
+++ web/src/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const session = require("express-session")
+const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require('path');
+const morgan = require("morgan");
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '..', 'public'));
+
 app.get('/', (req, res) => { 
-    console.log('hello');
-})
+    res.render('index', {
+        user: "test"
+        // user: req.user
+    });
+});
 
 module.exports = app;
