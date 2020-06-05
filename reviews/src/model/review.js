d94d0486a55942cee8348eeced25224b0eb4e08a
dbeezt <dbrereton1995@gmail.com>
2020-06-05 20:54:16 +0100

    Reconfigured model to have stronger ties to its associated Movie, and added content field for an actual written review

--- reviews/src/model/review.js
+++ reviews/src/model/review.js
@@ -3,8 +3,11 @@ const { v4: uuidv4 } = require('uuid');
 
 const ReviewSchema = new mongoose.Schema({
     _id: { type: String, default: uuidv4()},
+    movie: String,
     author: String,
+    content: String,
     rating: { type: Number, required: false, min: 0.0, max: 10.0 },
+    movieID: { type: String },
     postDate: { type: Date, default: Date.now},
 });
 
