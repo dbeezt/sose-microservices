c5876c764b52f53d81db44b52ceaa6c514faabc3
dbeezt <dbrereton1995@gmail.com>
2020-06-05 20:47:46 +0100

    Configured routes and views

--- movies/src/app.js
+++ movies/src/app.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const express = require('express');
+const path = require('path');
 const app = express();
 const MovieController = require('./controller/movie');
 
@@ -24,14 +25,25 @@ app.use(cookieSession(
      keys: ['user', 'guest']}
 ));
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'movies' });
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// app.use('/res', express.static(__dirname + '../resources'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
+        return res.status(200).json({});
+    };
+    next();
 });
 
-app.get('/movies', MovieController.ListAllMovies);
-app.get('/movie/:title', MovieController.ListOneMovie);
-app.post('/movie/add', MovieController.AddMovie);
-app.post('/movie/update/:title', MovieController.UpdateMovie);
-app.post('/movie/delete/:title', MovieController.DeleteMovie);
+app.get('/', MovieController.ListAllMovies);
+app.get('/search', MovieController.LoadSearchPage);
+app.post('/search', MovieController.ListOneMovie);
+app.post('/movie', MovieController.AddMovie);
+app.post('/update/:title', MovieController.UpdateMovie);
+app.post('/delete/:title', MovieController.DeleteMovie);
 
 module.exports = app;
